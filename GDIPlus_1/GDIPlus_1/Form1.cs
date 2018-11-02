ff2160cf0209b3dd1dba5dc9d623b83ea3550fa3
thanaponyalan <thanaponyalan@gmail.com>
2018-11-02 15:57:32 +0700

    TextureBrush

--- GDIPlus_1/GDIPlus_1/Form1.cs
+++ GDIPlus_1/GDIPlus_1/Form1.cs
@@ -19,22 +19,11 @@ namespace GDIPlus_1
 
         private void Form1_Paint(object sender, PaintEventArgs e)
         {
-            this.SetClientSizeCore(500,600);
             Graphics g = e.Graphics;
-            HatchBrush brush;
-            int x = 20;
-            int y = 20;
-            foreach (HatchStyle brushStype in Enum.GetValues(typeof(HatchStyle)))
-            {
-                brush = new HatchBrush(brushStype,Color.Navy,Color.Yellow);
-                g.FillRectangle(brush,x,y,40,20);
-                y += 30;
-                if ((y+30)>this.ClientSize.Height)
-                {
-                    y = 20;
-                    x += 150;
-                }
-            }
+            Image image = Image.FromFile("d:\\water.PNG");
+            TextureBrush brush = new TextureBrush(image);
+            Rectangle rect = new Rectangle(10,10,180,150);
+            g.FillEllipse(brush,rect);
             g.Dispose();
         }
     }

4b70d8a68f7dd7458e6d51bd2a460c70b37c1928
thanaponyalan <thanaponyalan@gmail.com>
2018-11-02 14:47:26 +0700

    withBrush

--- GDIPlus_1/GDIPlus_1/Form1.cs
+++ GDIPlus_1/GDIPlus_1/Form1.cs
@@ -20,13 +20,11 @@ namespace GDIPlus_1
         private void Form1_Paint(object sender, PaintEventArgs e)
         {
             Graphics g = e.Graphics;
-            Pen mypen = new Pen(Color.Black,1);
-            mypen.DashStyle = DashStyle.Dash;
-            g.DrawRectangle(mypen,10,10,200,200);
-            mypen.Width = 4;
-            mypen.Color = Color.Pink;
+            Brush mybrush = new SolidBrush(Color.DarkGreen);
+            Pen mypen = new Pen(mybrush,5);
             g.DrawEllipse(mypen,10,10,200,200);
             mypen.Dispose();
+            mybrush.Dispose();
         }
     }
 }

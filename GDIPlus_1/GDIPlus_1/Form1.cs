43c225b6dc45faf61fb983db65f3c81402835bd1
thanaponyalan <thanaponyalan@gmail.com>
2018-11-02 15:21:29 +0700

    Once a Rect

--- GDIPlus_1/GDIPlus_1/Form1.cs
+++ GDIPlus_1/GDIPlus_1/Form1.cs
@@ -20,25 +20,11 @@ namespace GDIPlus_1
         private void Form1_Paint(object sender, PaintEventArgs e)
         {
             Graphics g = e.Graphics;
-            GraphicsPath gp = new GraphicsPath();
+            Pen myPen = new Pen(Color.Blue,3);
+            g.DrawRectangle(myPen,10,120,100,100);
 
-            gp.AddCurve(new Point[] {
-                new Point(100,50),
-                new Point(105,40),
-                new Point(120,40),
-                new Point(130,65),
-                new Point(100,100)
-            },0.5f);
-
-            gp.AddCurve(new Point[] {
-                new Point(100,100),
-                new Point(70,65),
-                new Point(80,40),
-                new Point(95,40),
-                new Point(100,50)
-            },0.5f);
-
-            g.DrawPath(new Pen(Color.Red,3),gp);
+            Rectangle rect = new Rectangle(10,10,100,100);
+            g.DrawRectangle(myPen,rect);
             g.Dispose();
         }
     }

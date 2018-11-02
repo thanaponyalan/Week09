b530bb7da8a7d78de0b38ce5bbedd69c712f4d81
thanaponyalan <thanaponyalan@gmail.com>
2018-11-02 15:03:11 +0700

    endOfLine

--- GDIPlus_1/GDIPlus_1/Form1.cs
+++ GDIPlus_1/GDIPlus_1/Form1.cs
@@ -20,13 +20,29 @@ namespace GDIPlus_1
         private void Form1_Paint(object sender, PaintEventArgs e)
         {
             Graphics g = e.Graphics;
-            Pen pen = new Pen(Color.Green,2);
-            pen.DashStyle = DashStyle.DashDot;
-            e.Graphics.DrawLine(pen, 10, 80, 220, 80);
-            pen.Dispose();
-            pen = new Pen(Brushes.DeepSkyBlue,4);
-            e.Graphics.DrawLine(pen, 10, 120, 220, 120);
-            pen.Dispose();
+            Pen[] objPen = new Pen[11];
+            for (int i=0;i<11;i++)
+            {
+                objPen[i] = new Pen(Color.Blue,9);
+            }
+            objPen[0].EndCap = LineCap.AnchorMask;
+            objPen[1].EndCap = LineCap.ArrowAnchor;
+            objPen[2].EndCap = LineCap.Custom;
+            objPen[3].EndCap = LineCap.DiamondAnchor;
+            objPen[4].EndCap = LineCap.Flat;
+            objPen[5].EndCap = LineCap.NoAnchor;
+            objPen[6].EndCap = LineCap.Round;
+            objPen[7].EndCap = LineCap.RoundAnchor;
+            objPen[8].EndCap = LineCap.Square;
+            objPen[9].EndCap = LineCap.SquareAnchor;
+            objPen[10].EndCap = LineCap.Triangle;
+
+            for (int i=0;i<11;i++)
+            {
+                g.DrawLine(objPen[i],10,10+20*i,200,10+20*i);
+                objPen[i].Dispose();
+            }
+            g.Dispose();
         }
     }
 }
